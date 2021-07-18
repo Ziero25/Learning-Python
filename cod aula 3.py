3f860196fb07dd0a6208bf408d94c9312d1fe109
Yoil25 <54515278+Ziero25@users.noreply.github.com>
2021-07-18 00:55:29 -0300

    Delete cod aula 3.py

--- cod aula 3.py
+++
@@ -1,8 +0,0 @@
-nome = input("Digite seu nome: ")
-# pede uma informação ao usuario input
-print (nome)
-sobrenome = input("Agora, seu sobrenome: ")
-
-nomeCompleto = nome + " " + sobrenome
-print(nomeCompleto)
-
